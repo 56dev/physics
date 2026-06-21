97c08cd0811da9452e537be28c48d3676f11a286
56 dev <programsym987@gmail.com>
2026-06-21 16:05:26 -0600

    re-added the velocity field in Body, recalculating from position and previous position every time.

--- include/core.h
+++ include/core.h
@@ -5,8 +5,11 @@ class Body {
     private:
         Vector2 previous_position;
     public:
-        Vector2 position;
-        Vector2 acceleration;
+        Vector2 position; //m
+        Vector2 velocity; //m/s
+        Vector2 acceleration; //m/s^2
         Body(Vector2 p_pos, Vector2 p_vel, Vector2 p_accel, float dt); 
         void update_position(float dt);
+        Vector2 get_velocity(float dt);
 };
+
--- src/core.cpp
+++ src/core.cpp
@@ -5,6 +5,8 @@ void Body::update_position(float dt) {
     Vector2 tp = position;
     position.x = 2*position.x - previous_position.x + acceleration.x * dt*dt;
     position.y = 2*position.y - previous_position.y + acceleration.y * dt*dt;
+    velocity.x = (position.x - previous_position.x) / dt;
+    velocity.y = (position.y - previous_position.y) / dt;
     previous_position = tp;
 }
 Body::Body(
@@ -13,7 +15,12 @@ Body::Body(
         Vector2 p_accel, 
         float dt) 
     : position{p_pos}, 
-    acceleration {p_accel}{
+    acceleration {p_accel}
+    velocity {p_vel} {
 
     previous_position = Vector2Subtract(position, Vector2Scale(p_vel, dt));
 };
+float Body::get_velocity(float dt) {
+    return (position - previous_position) / dt;
+}
+
